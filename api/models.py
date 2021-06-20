e83b939186d8ec57adcfb1f692c229d1240f0e35
arthur <sousanot.23@gmail.om>
2021-06-19 21:06:31 -0300

    adding migrates

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
