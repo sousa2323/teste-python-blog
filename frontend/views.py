208f2ace6c2ef0d1ad525907eeb7b71565b4c27f
arthur <sousanot.23@gmail.om>
2021-06-20 15:48:42 -0300

    requiring login

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from api.models import Post
 
 
 
-class IndexView(ListView):
+class IndexView(LoginRequiredMixin, ListView): 
     model = Post
     template_name = 'index.html'
     queryset = Post.objects.all().order_by('-id')
 
-class ArticleDetail(DetailView):
+class ArticleDetail(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'article_detail.html'    
