054a53c0fdfa6243539a2e4ab7a9b2a47f400497
arthur <sousanot.23@gmail.om>
2021-06-20 12:30:14 -0300

    adding account app

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'author', 'created_at', 'update')
