fb1f64886f9cee74a25922184ce1b37ccfd973d4
arthur <sousanot.23@gmail.om>
2021-06-19 21:19:50 -0300

    creating list function

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Post
