3cfc2d0d69c41cd28040d0b7f5bcc9655aadb4a1
arthur <sousanot.23@gmail.om>
2021-06-19 21:37:57 -0300

    creating list views

--- api/views.py
+++ api/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import TaskSerializer
+from .serializers import PostSerializer
 
 from .models import Post
+
+
+@api_view(['GET'])
+def postkList(request):
+	posts = Post.objects.all().order_by('-id')
+	serializer = PostSerializer(posts, many=True)
+	return Response(serializer.data)
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from api.views import postkList
 
-# Create your views here.
+
+class IndexView(ListView):
+    model = postkList
+    template_name = 'index.html'
+    queryset = postkList.objects.all().order_by('-id')
+    context_object_name = posts
