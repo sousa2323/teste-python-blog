13877678b5cfba47e0bcea1e46b570e5f1c18464
arthur <sousanot.23@gmail.om>
2021-06-21 11:19:22 -0300

    view routes

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import IndexView, ArticleDetail
 
-
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('detail/<int:pk>', ArticleDetail.as_view(), name='article-detail'),
